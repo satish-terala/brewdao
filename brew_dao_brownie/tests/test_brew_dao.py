778486f9e780b2f8e08af23b584fea3c05875923
Satish Terala <satishkt@gmail.com>
2022-05-13 16:39:17 -0500

    reset directory name for python mapping issues.

--- brew_dao_brownie/tests/test_brew_dao.py
+++ brew_dao_brownie/tests/test_brew_dao.py
@@ -9,9 +9,6 @@ def brew_dao():
     daoAdmin = accounts[0]
     return BrewDAO.deploy({'from': daoAdmin})
 
-
-
-
 def test_balances_after_deploy_are_in_owner_account(brew_dao):
     daoOwner = accounts[1]
     token_created  = brew_dao.createBrewDAO('ULVA', 'ULVA', initial_supply, daoOwner, True, {'from': accounts[0]})
