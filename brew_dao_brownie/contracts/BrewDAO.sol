6497f9b0e44751fdc0811c37a08a4fbb983c6117
Satish Terala <satishkt@gmail.com>
2022-05-13 16:38:29 -0500

    reset directory name for python mapping issues.

--- brew_dao_brownie/contracts/BrewDAO.sol
+++ brew_dao_brownie/contracts/BrewDAO.sol
@@ -10,8 +10,35 @@ interface BrewDAOInterface {
 contract BrewDAO is BrewDAOInterface {
 
     address daoAdmin;
+    string breweryName;
+    string brewerySymbol;
+    address breweryOwner;
+    BrewToken token;
 
     constructor(){
         daoAdmin = msg.sender;
+    }
+
+
+    function createBrewDAO(string calldata _breweryName, string calldata _brewerySymbol, uint _initialSupply, address _ownerAddress, bool _createTokens) public onlyDaoAdmin returns (bool){
+        breweryName = _breweryName;
+        brewerySymbol = _brewerySymbol;
+        breweryOwner = _ownerAddress;
+        if (_createTokens) {
+            token = new BrewToken(_initialSupply, _ownerAddress, _breweryName, _brewerySymbol);
+        }
+        return true;
+    }
+
+
+    function getTokenBalance(address _inAddress) public view returns (uint){
+        return token.balanceOf(_inAddress);
+    }
+
+
+
+    modifier onlyDaoAdmin(){
+        require(msg.sender == daoAdmin, "Only DAO Admin is allowed to call this method");
+        _;
     }
 }
---
+++ brew_dao_brownie/tests/test_brew_dao.py
@@ -0,0 +1,20 @@
+import pytest
+from brownie import BrewDAO, network, accounts
+
+initial_supply = 10000
+
+
+@pytest.fixture
+def brew_dao():
+    daoAdmin = accounts[0]
+    return BrewDAO.deploy({'from': daoAdmin})
+
+
+
+
+def test_balances_after_deploy_are_in_owner_account(brew_dao):
+    daoOwner = accounts[1]
+    token_created  = brew_dao.createBrewDAO('ULVA', 'ULVA', initial_supply, daoOwner, True, {'from': accounts[0]})
+    print(token_created)
+    token_balance = brew_dao.getTokenBalance(daoOwner.address)
+    assert token_balance == initial_supply
