dfd03456a339486656306064dbb94fafa7801a01
Satish Terala <satishkt@gmail.com>
2022-05-13 22:37:39 -0500

    Set up hardhat integration and gas strategies.

--- brew_dao_py/src/app/deployer.py
+++ brew_dao_py/src/app/deployer.py
@@ -13,3 +13,11 @@ router = APIRouter()
 async def deploy_brew_dao():
     deployer_account = get_account()
     contract = BROWNIE_PROJECT_REFERENCE.BrewDAO.deploy({'from': deployer_account})
+
+
+@router.get('/contracts/brewdao/create_tokens')
+async def create_tokens(brewery_name: str, brewery_symbol: str, init_supply: int, daoOwner: str, create_tokens: bool):
+    contract = BROWNIE_PROJECT_REFERENCE.BrewDAO[0]
+    deployer_account = get_account()
+    token_created = contract.createBrewDAO(brewery_name, brewery_symbol, init_supply, daoOwner, create_tokens,
+                                           {'from': deployer_account})
