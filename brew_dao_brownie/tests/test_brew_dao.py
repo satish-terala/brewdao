d2d260943e41e0944e2787ac02851fa4d832087a
Satish Terala <satishkt@gmail.com>
2022-05-13 21:22:27 -0500

    reset directory name for python mapping issues.

--- brew_dao_brownie/tests/test_brew_dao.py
+++ brew_dao_brownie/tests/test_brew_dao.py
@@ -1,18 +1,23 @@
-import pytest
 import brownie
-from brownie import BrewDAO, network, accounts
+import pytest
+from brownie import BrewDAO, accounts, Wei
+from brownie.network.gas.strategies import LinearScalingStrategy
 
 initial_supply = 10000
 
+base_gas_price  = LinearScalingStrategy(Wei(17597082), Wei(17597082*10), 1.1)
 
 @pytest.fixture
 def brew_dao():
+
     daoAdmin = accounts[0]
-    return BrewDAO.deploy({'from': daoAdmin})
+    return BrewDAO.deploy({'from': daoAdmin, "gas_price": base_gas_price})
+
 
 def test_balances_after_deploy_are_in_owner_account(brew_dao):
     daoOwner = accounts[1]
-    token_created  = brew_dao.createBrewDAO('ULVA', 'ULVA', initial_supply, daoOwner, True, {'from': accounts[0]})
+    token_created = brew_dao.createBrewDAO('ULVA', 'ULVA', initial_supply, daoOwner, True,
+                                           {'from': accounts[0], "gas_price": base_gas_price})
     print(token_created)
     token_balance = brew_dao.getTokenBalance(daoOwner.address)
     assert token_balance == initial_supply
@@ -20,8 +25,7 @@ def test_balances_after_deploy_are_in_owner_account(brew_dao):
 
 def test_create_brew_dao_can_only_be_called_by_dao_admin(brew_dao):
     daoOwner = accounts[1]
-    some_other_address=accounts[5]
+    some_other_address = accounts[5]
     with brownie.reverts("Only DAO Admin is allowed to call this method"):
-        brew_dao.createBrewDAO('ULVA', 'ULVA', initial_supply, daoOwner, True, {'from': some_other_address})
-
-
+        brew_dao.createBrewDAO('ULVA', 'ULVA', initial_supply, daoOwner, True,
+                               {'from': some_other_address, 'gas_price': base_gas_price})
